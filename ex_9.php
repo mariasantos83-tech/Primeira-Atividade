20204bb0aeb1223647febfd7ab8cd0a9fb2918af
MariaLoreto <maria_santos83@estudante.>
2026-08-07 11:28:36 -0300

    add

--- ex_9.php
+++ ex_9.php
@@ -1,1 +1,30 @@
 <?php
+function analisarNumero(num) {
+    const parOuImpar = num % 2 === 0 ? "Par" : "Ímpar";
+    let ePrimo = num > 1;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            ePrimo = false;
+            break;KT
+        }
+    }
+
+    let somaDivisores = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
+            somaDivisores += i;
+        }
+    }
+    const ePerfeito = num > 0 && somaDivisores === num;
+
+    return {
+        numero: num,
+        classificacao: parOuImpar,
+        ehPrimo: ePrimo ? "Sim" : "Não",
+        ehPerfeito: ePerfeito ? "Sim" : "Não"
+    };
+}
+
+console.log(analisarNumero(6));  
+console.log(analisarNumero(7));
+console.log(analisarNumero(28));
