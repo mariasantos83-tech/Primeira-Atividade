cb2fbafbf2f446a135ea8da5e8c7dd23b9f162ca
MariaLoreto <maria_santos83@estudante.>
2026-08-07 11:20:24 -0300

    add

--- ex_8.php
+++ ex_8.php
@@ -1,1 +1,10 @@
 <?php
+function ordenarNomes(listaNomes) {
+        return listaNomes
+        .split(',')
+        .map(nome => nome.trim())
+        .sort((a, b) => a.localeCompare(b));
+}
+const entrada = " Mariana, Carlos , ana, Bruno, Vitória ";
+console.log(ordenarNomes(entrada)); 
+
